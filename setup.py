379460597d4fbeaf397dffc3cb19f4f8d5f888fe
peekxc <matt.piekenbrock@gmail.com>
2022-07-27 21:31:51 -0400

    examples + tests

--- setup.py
+++ setup.py
@@ -1,12 +1,2 @@
-from setuptools import setup # find_packages
+from setuptools import setup
 setup()
-
-# setup(
-#   name='pyclique',
-#   version='0.1.0',
-#   description='Setting up a python package',
-#   url='https://github.com/peekxc/pyclique',
-#   package_dir={'': 'src'},
-#   packages='pyclique'
-# )
-# packages=find_packages(include=['src/pyclique'])
